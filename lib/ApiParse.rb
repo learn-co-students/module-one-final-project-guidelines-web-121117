2457e62e7744bd9d988a2a49bfde3be7615fd5af
julientregoat <julienctregoat@gmail.com>
2018-01-02 16:53:31 -0500

    refactored ApiParse methods

--- lib/ApiParse.rb
+++ lib/ApiParse.rb
@@ -4,23 +4,27 @@ module ApiParse
     JSON.parse(RestClient.get(url))
   end
 
-  def self.search_character_page(page=1)
+  def self.search_character_pages(page=1)
     JSON.parse(RestClient.get("https://anapioficeandfire.com/api/characters?page=#{page}&pageSize=50"))
   end
 
-  def self.get_all_characters
+  def self.find_or_create_allegiances(character_hash)
+    if !character["allegiances"].empty?
+      character["allegiances"].each do |house_url|
+        house_info = self.find_and_parse(house_url)
+        new_char.houses << House.find_or_create_by(name: house_info["name"], url: house_info["url"])
+      end
+    end
+    new_char.houses
+  end
+
+  def self.import_data
     page = 1
-    while !self.search_character_page.empty?
-      page_characters = self.search_character_page(page)
-      binding.pry
-      page_characters.each do |character|
+    while !self.search_character_pages(page).empty?
+      characters = self.search_character_pages(page)
+      characters.each do |character|
         new_char = Character.find_or_create_by(name: character["name"], url: character["url"])
-        if !character["allegiances"].empty?
-          character["allegiances"].each do |house_url|
-            house_info = self.find_and_parse(house_url)
-            new_char.houses << House.find_or_create_by(name: house_info["name"], url: house_info["url"])
-          end
-        end
+        self.find_or_create_character_allegiances(character)
       end
       page += 1
     end
