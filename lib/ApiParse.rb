1fd78f0d1f74be96f7fa048ac54b230e4da5754d
github username <github email address>
2018-01-02 15:58:22 -0500

    More commits. More pain

--- lib/ApiParse.rb
+++ lib/ApiParse.rb
@@ -10,16 +10,15 @@ module ApiParse
     while !JSON.parse(characters).empty?
       page_characters = JSON.parse(characters)
       page_characters.each do |character|
-        # binding.pry
         new_char = Character.find_or_create_by(name: character["name"], url: character["url"])
         if !character["allegiances"].empty?
           character["allegiances"].each do |house_url|
-            # binding.pry
             house_info = self.find_and_parse(house_url)
             new_char.houses << House.find_or_create_by(name: house_info["name"], url: house_info["url"])
           end
         end
       end
+      page += 1
     end
   end
 
