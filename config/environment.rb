8125927bbedafbab20a8a41c5d8200a623658e8d
julientregoat <julienctregoat@gmail.com>
2018-01-02 13:35:40 -0500

    added method to pull GoT API Characters

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,9 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
+require_all 'db'
+require 'open-uri'
+require 'json'
 
 old_logger = ActiveRecord::Base.logger
 ActiveRecord::Base.logger = nil
