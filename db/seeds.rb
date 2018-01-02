826787a47ba2ec97eb5459ce63745d203a01ba88
github username <github email address>
2018-01-02 15:23:49 -0500

    Sending stuff there up.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,15 @@
 
 module ApiParse
 
-def self.get_all_characters
-  page = 1
-  characters = RestClient.get("https://anapioficeandfire.com/api/characters?page=#{page}&pageSize=50")
-  while !JSON.parse(characters).empty?
-    page_characters = JSON.parse(characters)
-    page_characters.each do |character|
-      new_char = Character.find_or_create_by(name: character["name"], url: character["url"])
+  def self.get_all_characters
+    page = 1
+    characters = RestClient.get("https://anapioficeandfire.com/api/characters?page=#{page}&pageSize=50")
+    while !JSON.parse(characters).empty?
+      page_characters = JSON.parse(characters)
+      page_characters.each do |character|
+        new_char = Character.find_or_create_by(name: character["name"], url: character["url"])
+      end
     end
   end
-end
+  
 end
