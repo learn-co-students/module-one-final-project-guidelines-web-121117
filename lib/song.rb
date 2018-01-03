62ee2e06c99157dd08994b24164b18f9a9a50da9
johneckert <johnteckert@gmail.com>
2018-01-03 11:51:21 -0500

    added tolerance to float parameter selection

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,14 @@ class Song < ActiveRecord::Base
   has_many :playlists, through: :song_playlists
 
   def self.select_by(parameter, value)
-    self.all.select do |song|
-      song[parameter.to_sym] == value
+    if value.is_a?(Float)
+      self.all.select do |song|
+        song[parameter.to_sym] >= value - 0.05 && song[parameter.to_sym] <= value + 0.05
+      end
+    else
+      self.all.select do |song|
+        song[parameter.to_sym] == value
+      end
     end
   end
 
