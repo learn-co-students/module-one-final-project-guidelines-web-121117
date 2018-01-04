26e7ee8121a04d9b6d3edf5857c80b4d92503505
github username <github email address>
2018-01-04 11:46:56 -0500

    added hangman picture

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -20,23 +20,27 @@ class Hangman
   end
 
   def greet
-    puts "Welcome to Movie Hangman!"
+    puts ""
+    puts "Welcome to Movie Hangman!".colorize(:color=>:green, :mode=>:bold)
   end
 
   def turn
     while true
       puts ""
-      puts "You have #{@lives} lives remaining!"
-      puts "Used letters: " + @used_letters.map(&:upcase).join(" ")
+      puts "You have #{@lives} lives remaining!".colorize(:color=>:red)
+      puts "Used letters: " + @used_letters.map(&:upcase).join(" ").colorize(:color=>:red)
       puts ""
-      puts "Current Movie State: " + check_movie(@movie_name)
+      puts "Current Movie State: ".colorize(:mode=>:bold) + check_movie(@movie_name)
       puts ""
-      puts "Please enter a letter:"
+      puts "Please enter a letter:".colorize(:mode=>:bold)
+
       letter = gets.chomp.downcase
       return main_menu if letter == "exit"
+
       if !@movie_name.chars.include?(letter) || !@alphabet.include?(letter)
         puts "You entered an incorrect letter, YOU LOSE A LIFE!"
         @lives -= 1
+        hangman_man
       end
 
       if @used_letters.include?(letter)
@@ -51,14 +55,18 @@ class Hangman
         break
       end
 
-      @used_letters << letter
+      @used_letters << letter unless @used_letters.include?(letter)
 
       if check_movie(@movie_name).count("#") == 0
         puts ""
-        puts check_movie(@movie_name)
+        puts "٩(◕‿◕｡)۶".colorize(:color=>:magenta)
         puts ""
-        puts "Congrats! You win!!!"
-        break
+        puts check_movie(@movie_name).colorize(:color=>:cyan, :mode=>:bold)
+        puts ""
+        puts "Congrats! You win!!!".colorize(:color=>:magenta, :mode=>:bold)
+        puts ""
+        puts "٩(◕‿◕｡)۶".colorize(:color=>:magenta)
+        return main_menu
       end
 
       puts ""
@@ -67,10 +75,84 @@ class Hangman
     end
   end
 
+  def hangman_man
+    case @lives
+      when 6
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "               |"
+        puts "               |"
+        puts "               |"
+        puts "               |"
+        puts "               |"
+        puts "    ============"
+      when 5
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "      0        |"
+        puts "               |"
+        puts "               |"
+        puts "               |"
+        puts "               |"
+        puts "    ============"
+      when 4
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "      0        |"
+        puts "      |        |"
+        puts "      |        |"
+        puts "               |"
+        puts "               |"
+        puts "    ============"
+      when 3
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "      0        |"
+        puts "      |/       |"
+        puts "      |        |"
+        puts "               |"
+        puts "               |"
+        puts "    ============"
+      when 2
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "      0        |"
+        puts "     \\|/       |"
+        puts "      |        |"
+        puts "               |"
+        puts "               |"
+        puts "    ============"
+      when 1
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "      0        |"
+        puts "     \\|/       |"
+        puts "      |        |"
+        puts "     /         |"
+        puts "               |"
+        puts "    ============"
+      when 0
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "      0        |"
+        puts "     \\|/       |"
+        puts "      |        |"
+        puts "     / \\       |"
+        puts "               |"
+        puts "    ============"
+    end
+  end
+
   def run
     greet
     turn
   end
-
 
 end
