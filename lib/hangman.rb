102eaa6efbec86e2d3547242827828e63b79f227
github username <github email address>
2018-01-04 14:35:38 -0500

    hangman with blue

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,52 +1,62 @@
 class Hangman
   include HelperMethods
   attr_accessor :movie_name
+
   def initialize
     @lives = 6
     @used_letters = []
     @alphabet = ("a".."z").to_a
-    @movie_name = Movie.all.map { |inst| inst.name }.sample.downcase
+    @movie_name = Movie.all.map { |movie_instance| movie_instance.name }.sample.downcase
   end
+
   def check_movie(movie_name)
     words = movie_name.downcase.split
     words = words.map do |word|
       word.chars.map do |char|
-        @used_letters.include?(char) ? char : "#"
+        @used_letters.include?(char) || !@alphabet.include?(char) ? char : "#"
       end.join.capitalize
     end
     words.join(" ")
   end
+
   def greet
     puts ""
     puts "Welcome to Movie Hangman!".colorize(:color=>:green, :mode=>:bold)
   end
+
   def turn
     while true
       puts ""
-      # puts "You have " + "#{@lives}" + " lives remaining!"
       puts "You have #{@lives} lives remaining!".colorize(:color=>:red)
       puts "Used letters: " + @used_letters.map(&:upcase).join(" ").colorize(:color=>:red)
       puts ""
       puts "Current Movie State: ".colorize(:mode=>:bold) + check_movie(@movie_name)
       puts ""
       puts "Please enter a letter:".colorize(:mode=>:bold)
+
       letter = gets.chomp.downcase
       return main_menu if letter == "exit"
+
       if !@movie_name.chars.include?(letter) || !@alphabet.include?(letter)
         puts "You entered an incorrect letter, YOU LOSE A LIFE!"
         @lives -= 1
+        hangman_man
       end
+
       if @used_letters.include?(letter)
         puts "You already used this letter, please enter another one!"
         puts ""
       end
+
       if @lives == 0
         puts ""
         puts "You ran out of lives!"
         puts "The movie was: " + @movie_name.split.map(&:capitalize).join(" ")
-        break
+        return main_menu
       end
-      @used_letters << letter
+
+      @used_letters << letter unless @used_letters.include?(letter)
+
       if check_movie(@movie_name).count("#") == 0
         puts ""
         puts "٩(◕‿◕｡)۶".colorize(:color=>:magenta)
@@ -58,13 +68,91 @@ class Hangman
         puts "٩(◕‿◕｡)۶".colorize(:color=>:blue)
         return main_menu
       end
+
       puts ""
       puts check_movie(@movie_name)
       puts ""
     end
   end
+
+  def hangman_man
+    case @lives
+      when 6
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "               |"
+        puts "               |"
+        puts "               |"
+        puts "               |"
+        puts "               |"
+        puts "    ============"
+      when 5
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "      0        |"
+        puts "               |"
+        puts "               |"
+        puts "               |"
+        puts "               |"
+        puts "    ============"
+      when 4
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "      0        |"
+        puts "      |        |"
+        puts "      |        |"
+        puts "               |"
+        puts "               |"
+        puts "    ============"
+      when 3
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "      0        |"
+        puts "      |/       |"
+        puts "      |        |"
+        puts "               |"
+        puts "               |"
+        puts "    ============"
+      when 2
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "      0        |"
+        puts "     \\|/       |"
+        puts "      |        |"
+        puts "               |"
+        puts "               |"
+        puts "    ============"
+      when 1
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "      0        |"
+        puts "     \\|/       |"
+        puts "      |        |"
+        puts "     /         |"
+        puts "               |"
+        puts "    ============"
+      when 0
+        puts ""
+        puts "      __________"
+        puts "      |        |"
+        puts "      0        |"
+        puts "     \\|/       |"
+        puts "      |        |"
+        puts "     / \\       |"
+        puts "               |"
+        puts "    ============"
+    end
+  end
+
   def run
     greet
     turn
   end
+
 end
