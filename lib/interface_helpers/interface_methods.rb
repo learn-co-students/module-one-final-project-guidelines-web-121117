048b3d99ae616b5bb41d7c50050efe0b438f5e63
github username <github email address>
2018-01-05 08:39:50 -0500

    Updated lanaguage for Find Character, Find House, Find Region, and Find Book methods

--- lib/interface_helpers/interface_methods.rb
+++ lib/interface_helpers/interface_methods.rb
@@ -24,62 +24,53 @@ G:::::G    GGGGG::::G    O:::::O     O:::::O          T:::::T
   end
 
   def good_bye
-    banner = "
-WWWWWWWW                           WWWWWWWW  IIIIIIIIII  NNNNNNNN        NNNNNNNN  TTTTTTTTTTTTTTTTTTTTTTT  EEEEEEEEEEEEEEEEEEEEEE  RRRRRRRRRRRRRRRRR
-W::::::W                           W::::::W  I::::::::I  N:::::::N       N::::::N  T:::::::::::::::::::::T  E::::::::::::::::::::E  R::::::::::::::::R
-W::::::W                           W::::::W  I::::::::I  N::::::::N      N::::::N  T:::::::::::::::::::::T  E::::::::::::::::::::E  R::::::RRRRRR:::::R
-W::::::W                           W::::::W  II::::::II  N:::::::::N     N::::::N  T:::::TT:::::::TT:::::T  EE::::::EEEEEEEEE::::E  RR:::::R     R:::::R
- W:::::W           WWWWW           W:::::W     I::::I    N::::::::::N    N::::::N  TTTTTT  T:::::T  TTTTTT    E:::::E       EEEEEE    R::::R     R:::::R
-  W:::::W         W:::::W         W:::::W      I::::I    N:::::::::::N   N::::::N          T:::::T            E:::::E                 R::::R     R:::::R
-   W:::::W       W:::::::W       W:::::W       I::::I    N:::::::N::::N  N::::::N          T:::::T            E::::::EEEEEEEEEE       R::::RRRRRR:::::R
-    W:::::W     W:::::::::W     W:::::W        I::::I    N::::::N N::::N N::::::N          T:::::T            E:::::::::::::::E       R:::::::::::::RR
-     W:::::W   W:::::W:::::W   W:::::W         I::::I    N::::::N  N::::N:::::::N          T:::::T            E:::::::::::::::E       R::::RRRRRR:::::R
-      W:::::W W:::::W W:::::W W:::::W          I::::I    N::::::N   N:::::::::::N          T:::::T            E::::::EEEEEEEEEE       R::::R     R:::::R
-       W:::::W:::::W   W:::::W:::::W           I::::I    N::::::N    N::::::::::N          T:::::T            E:::::E                 R::::R     R:::::R
-        W:::::::::W     W:::::::::W            I::::I    N::::::N     N:::::::::N          T:::::T            E:::::E       EEEEEE    R::::R     R:::::R
-         W:::::::W       W:::::::W           II::::::II  N::::::N      N::::::::N        TT:::::::TT        EE::::::EEEEEEEE:::::E  RR:::::R     R:::::R
-          W:::::W         W:::::W            I::::::::I  N::::::N       N:::::::N        T:::::::::T        E::::::::::::::::::::E  R::::::R     R:::::R
-           W:::W           W:::W             I::::::::I  N::::::N        N::::::N        T:::::::::T        E::::::::::::::::::::E  R::::::R     R:::::R
-            WWW             WWW              IIIIIIIIII  NNNNNNNN         NNNNNNN        TTTTTTTTTTT        EEEEEEEEEEEEEEEEEEEEEE  RRRRRRRR     RRRRRRR
+    b = "
 
-                                                                    IIIIIIIIII     SSSSSSSSSSSSSSS
-                                                                    I::::::::I   SS:::::::::::::::S
-                                                                    I::::::::I  S:::::SSSSSS::::::S
-                                                                    II::::::II  S:::::S     SSSSSSS
-                                                                      I::::I    S:::::S
-                                                                      I::::I    S:::::S
-                                                                      I::::I     S::::SSSS
-                                                                      I::::I      SS::::::SSSSS
-                                                                      I::::I        SSS::::::::SS
-                                                                      I::::I           SSSSSS::::S
-                                                                      I::::I                S:::::S
-                                                                      I::::I                S:::::S
-                                                                    II::::::II  SSSSSSS     S:::::S
-                                                                    I::::::::I  S::::::SSSSSS:::::S
-                                                                    I::::::::I  S:::::::::::::::SS
-                                                                    IIIIIIIIII   SSSSSSSSSSSSSSS
 
-                  CCCCCCCCCCCCC       OOOOOOOOO       MMMMMMMM               MMMMMMMM  IIIIIIIIII  NNNNNNNN        NNNNNNNN          GGGGGGGGGGGGG
-               CCC::::::::::::C     OO:::::::::OO     M:::::::M             M:::::::M  I::::::::I  N:::::::N       N::::::N       GGG::::::::::::G
-             CC:::::::::::::::C   OO:::::::::::::OO   M::::::::M           M::::::::M  I::::::::I  N::::::::N      N::::::N     GG:::::::::::::::G
-            C:::::CCCCCCCC::::C  O:::::::OOO:::::::O  M:::::::::M         M:::::::::M  II::::::II  N:::::::::N     N::::::N    G:::::GGGGGGGG::::G
-           C:::::C       CCCCCC  O::::::O   O::::::O  M::::::::::M       M::::::::::M    I::::I    N::::::::::N    N::::::N   G:::::G       GGGGGG
-          C:::::C                O:::::O     O:::::O  M:::::::::::M     M:::::::::::M    I::::I    N:::::::::::N   N::::::N  G:::::G
-          C:::::C                O:::::O     O:::::O  M:::::::M::::M   M::::M:::::::M    I::::I    N:::::::N::::N  N::::::N  G:::::G
-          C:::::C                O:::::O     O:::::O  M::::::M M::::M M::::M M::::::M    I::::I    N::::::N N::::N N::::::N  G:::::G    GGGGGGGGGG
-          C:::::C                O:::::O     O:::::O  M::::::M  M::::M::::M  M::::::M    I::::I    N::::::N  N::::N:::::::N  G:::::G    G::::::::G
-          C:::::C                O:::::O     O:::::O  M::::::M   M:::::::M   M::::::M    I::::I    N::::::N   N:::::::::::N  G:::::G    GGGGG::::G
-          C:::::C                O:::::O     O:::::O  M::::::M    M:::::M    M::::::M    I::::I    N::::::N    N::::::::::N  G:::::G        G::::G
-           C:::::C       CCCCCC  O::::::O   O::::::O  M::::::M     MMMMM     M::::::M    I::::I    N::::::N     N:::::::::N   G:::::G       G::::G
-            C:::::CCCCCCCC::::C  O:::::::OOO:::::::O  M::::::M               M::::::M  II::::::II  N::::::N      N::::::::N    G:::::GGGGGGGG::::G
-             CC:::::::::::::::C   OO:::::::::::::OO   M::::::M               M::::::M  I::::::::I  N::::::N       N:::::::N     GG:::::::::::::::G
-               CCC::::::::::::C     OO:::::::::OO     M::::::M               M::::::M  I::::::::I  N::::::N        N::::::N       GGG::::::GGG:::G
-                  CCCCCCCCCCCCC       OOOOOOOOO       MMMMMMMM               MMMMMMMM  IIIIIIIIII  NNNNNNNN         NNNNNNN          GGGGGG   GGGG
+    
+           WWWWWWWW                           WWWWWWWW iiii                            tttt
+           W::::::W                           W::::::Wi::::i                        ttt:::t
+           W::::::W                           W::::::W iiii                         t:::::t
+           W::::::W                           W::::::W                              t:::::t
+            W:::::W           WWWWW           W:::::Wiiiiiiinnnn  nnnnnnnn    ttttttt:::::ttttttt        eeeeeeeeeeee    rrrrr   rrrrrrrrr
+             W:::::W         W:::::W         W:::::W i:::::in:::nn::::::::nn  t:::::::::::::::::t      ee::::::::::::ee  r::::rrr:::::::::r
+              W:::::W       W:::::::W       W:::::W   i::::in::::::::::::::nn t:::::::::::::::::t     e::::::eeeee:::::eer:::::::::::::::::r
+               W:::::W     W:::::::::W     W:::::W    i::::inn:::::::::::::::ntttttt:::::::tttttt    e::::::e     e:::::err::::::rrrrr::::::r
+                W:::::W   W:::::W:::::W   W:::::W     i::::i  n:::::nnnn:::::n      t:::::t          e:::::::eeeee::::::e r:::::r     r:::::r
+                 W:::::W W:::::W W:::::W W:::::W      i::::i  n::::n    n::::n      t:::::t          e:::::::::::::::::e  r:::::r     rrrrrrr
+                  W:::::W:::::W   W:::::W:::::W       i::::i  n::::n    n::::n      t:::::t          e::::::eeeeeeeeeee   r:::::r
+                   W:::::::::W     W:::::::::W        i::::i  n::::n    n::::n      t:::::t    tttttte:::::::e            r:::::r
+                    W:::::::W       W:::::::W        i::::::i n::::n    n::::n      t::::::tttt:::::te::::::::e           r:::::r
+                     W:::::W         W:::::W         i::::::i n::::n    n::::n      tt::::::::::::::t e::::::::eeeeeeee   r:::::r
+                      W:::W           W:::W          i::::::i n::::n    n::::n        tt:::::::::::tt  ee:::::::::::::e   r:::::r
+                       WWW             WWW           iiiiiiii nnnnnn    nnnnnn          ttttttttttt      eeeeeeeeeeeeee   rrrrrrr
+
+
+           IIIIIIIIII                              CCCCCCCCCCCCC                                           iiii
+           I::::::::I                           CCC::::::::::::C                                          i::::i
+           I::::::::I                         CC:::::::::::::::C                                           iiii
+           II::::::II                        C:::::CCCCCCCC::::C
+             I::::I      ssssssssss         C:::::C       CCCCCC   ooooooooooo      mmmmmmm    mmmmmmm   iiiiiiinnnn  nnnnnnnn       ggggggggg   ggggg
+             I::::I    ss::::::::::s       C:::::C               oo:::::::::::oo  mm:::::::m  m:::::::mm i:::::in:::nn::::::::nn    g:::::::::ggg::::g
+             I::::I  ss:::::::::::::s      C:::::C              o:::::::::::::::om::::::::::mm::::::::::m i::::in::::::::::::::nn  g:::::::::::::::::g
+             I::::I  s::::::ssss:::::s     C:::::C              o:::::ooooo:::::om::::::::::::::::::::::m i::::inn:::::::::::::::ng::::::ggggg::::::gg
+             I::::I   s:::::s  ssssss      C:::::C              o::::o     o::::om:::::mmm::::::mmm:::::m i::::i  n:::::nnnn:::::ng:::::g     g:::::g
+             I::::I     s::::::s           C:::::C              o::::o     o::::om::::m   m::::m   m::::m i::::i  n::::n    n::::ng:::::g     g:::::g
+             I::::I        s::::::s        C:::::C              o::::o     o::::om::::m   m::::m   m::::m i::::i  n::::n    n::::ng:::::g     g:::::g
+             I::::I  ssssss   s:::::s       C:::::C       CCCCCCo::::o     o::::om::::m   m::::m   m::::m i::::i  n::::n    n::::ng::::::g    g:::::g
+           II::::::IIs:::::ssss::::::s       C:::::CCCCCCCC::::Co:::::ooooo:::::om::::m   m::::m   m::::mi::::::i n::::n    n::::ng:::::::ggggg:::::g
+           I::::::::Is::::::::::::::s         CC:::::::::::::::Co:::::::::::::::om::::m   m::::m   m::::mi::::::i n::::n    n::::n g::::::::::::::::g
+           I::::::::I s:::::::::::ss            CCC::::::::::::C oo:::::::::::oo m::::m   m::::m   m::::mi::::::i n::::n    n::::n  gg::::::::::::::g
+           IIIIIIIIII  sssssssssss                 CCCCCCCCCCCCC   ooooooooooo   mmmmmm   mmmmmm   mmmmmmiiiiiiii nnnnnn    nnnnnn    gggggggg::::::g
+                                                                                                                                              g:::::g
+                                                                                                                                  gggggg      g:::::g
+                                                                                                                                  g:::::gg   gg:::::g
+                                                                                                                                   g::::::ggg:::::::g
+                                                                                                                                    gg:::::::::::::g
+                                                                                                                                      ggg::::::ggg
+                                                                                                                                         gggggg
 "
-
-    puts banner.colorize(:blue)
-    puts "\nUntil next time...".colorize(:color => :red)
-    puts "\n"
+    puts b.colorize(:blue)
   end
 
   def all_characters
@@ -118,7 +109,7 @@ W::::::W                           W::::::W  II::::::II  N:::::::::N     N::::::
     puts "\nHere are all the characters in the GoT universe:"
     puts "\n"
     puts all_characters
-    puts "\nType which character you are looking for here:"
+    puts "\nType the full name, or the first few letters of the character you are looking for here:"
   end
 
   def find_character(input)
@@ -163,7 +154,7 @@ W::::::W                           W::::::W  II::::::II  N:::::::::N     N::::::
     puts "\nHere are all the houses in the GoT universe:"
     puts "\n"
     puts all_houses
-    puts "\nType which house you are looking for here:"
+    puts "\nType the full name, or the first few letters of the house you are looking for here:"
   end
 
   def find_house(input)
@@ -201,7 +192,7 @@ W::::::W                           W::::::W  II::::::II  N:::::::::N     N::::::
       puts "\nHere are all the houses written for the GoT universe:"
       puts "\n"
       puts all_books
-      puts "\nType which book you are looking for here:"
+      puts "\nType the full name, or the first few letters of the book looking for here:"
   end
 
   def find_book(input)
@@ -230,7 +221,7 @@ W::::::W                           W::::::W  II::::::II  N:::::::::N     N::::::
       puts "\nHere are all the regions in the GoT universe:"
       puts "\n"
       puts all_regions
-      puts "\nType which region you are looking for here:"
+      puts "\nType the full name, or the first few letters of the region looking for here:"
   end
 
   def find_region(input)
@@ -299,7 +290,5 @@ W::::::W                           W::::::W  II::::::II  N:::::::::N     N::::::
     puts "Once deleted, you can rerun the program which will download a new database for you."
     puts "\n"
   end
-
-
 
 end
